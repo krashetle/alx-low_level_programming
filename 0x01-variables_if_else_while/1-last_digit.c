6987508291e8abcab936faf397abd6795e65b5cb
Kgopotso <kgopotsorashetle@gmail.com>
2022-12-19 22:00:50 -0800

    last gigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	} else
-	{	
+	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
 	return (0);
